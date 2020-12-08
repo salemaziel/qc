e274f8394191d728ae67d680be5d6d7d67c18ba3
salemaziel <mymainemail0501@gmail.com>
2020-12-07 18:40:57 -0800

    oprimzed jpg sizes

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        whitelist: ['body.is-preload *',' body.is-preload *:before',' body.is-preload *:after','NoDesktop', 'NoMobile'], // Don't remove this selector
+        whitelist: ['body.is-preload *',' body.is-preload *:before',' body.is-preload *:after','NoDesktop', 'NoMobile', 'bg-white', 'bg-lbrown', 'bg-dbrown',], // Don't remove this selector
          whitelistPatterns: [/btn/, /container/, /row/, /col/, /card/, /body/, /form/, /input/, /NoDesktop/, /is-preload/, /nav/, /navbar/  ],
          ignore: ['/Section.scss', '/footer.module.css', 'layout.css' ], // Ignore files/folders
         // purgeOnly : ['bootstrap/'], // Purge only these files/folders
